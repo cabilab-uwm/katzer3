8baea11c990e60c1c102bb069a0aca9ab9df44f8
cabilab-uwm <146369698+cabilab-uwm@users.noreply.github.com>
2023-10-04 17:25:55 -0500

    Delete 15 directory

--- 15/run_category_class.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem=4000 --output=logs/15_mvpa_%j.txt 
-
-source ~/Data/conda/bin/activate ''
-conda activate dicat_RSA
-
-python category_class.py 
--- 15/run_fame_effect_class.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem=4000 --output=logs/15_fame_effect_mvpa_%j.txt 
-
-source ~/Data/conda/bin/activate ''
-conda activate dicat_RSA
-
-python fame_effect_class.py 
